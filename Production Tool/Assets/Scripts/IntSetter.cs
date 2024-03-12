748bad460b6c592b5d29982753048d5d5c13b664
Sven Wels <svenvonwelzenstein@gmail.com>
2024-03-12 13:49:43 +0100

    Added first UI functions

---
+++ Production Tool/Assets/Scripts/IntSetter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IntSetter : MonoBehaviour
+{
+    private int value;
+    public UnityEvent<int> OnValueChanged;
+
+    public void SetIntFromString(string str)
+    {
+        try { value = int.Parse(str); }
+        catch { value = 1; }
+        OnValueChanged?.Invoke(value);
+    }
+}

671ffc910e4e1a99082f06e77afcf285047a63ef
Sven Wels <svenvonwelzenstein@gmail.com>
2024-06-29 20:31:16 +0200

    Fixed hover text

--- Production Tool/Assets/Scripts/HoverText.cs
+++ Production Tool/Assets/Scripts/HoverText.cs
@@ -1,31 +1,32 @@
 using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HoverText : Selectable
+public class HoverText : MonoBehaviour
 {
+    private Bounds bounds;
     [SerializeField] private GameObject hoverTextPrefab;
     private GameObject currentHoverText;
     public string content;
-    [SerializeField] private float maxWidth = 600;
     public static Transform hoverTextParent;
+    public LayerMask hitboxLayer;
 
     private bool hoverActive = false;
     private float hoverTime = 0;
     public float timeThreshold = 1.0f;
 
-    protected override void Awake()
+    private void Awake()
     {
-        base.Awake();
+        bounds = GetComponent<BoxCollider>().bounds;
         if (hoverTextParent != null) { return; }
         hoverTextParent = FindObjectOfType<HoverTextParent>().transform;
     }
 
     private void Update()
     {
-        Debug.Log(IsHighlighted());
-        if (IsHighlighted())
+        if (IsHoveredOver())
         {
             if(Input.GetMouseButtonDown(0))
             {
@@ -50,6 +51,7 @@ public class HoverText : Selectable
             Destroy(currentHoverText);
             return;
         }
+        hoverActive = false;
         hoverTime = 0;
     }
 
@@ -58,12 +60,29 @@ public class HoverText : Selectable
         currentHoverText = Instantiate(hoverTextPrefab, hoverTextParent);
         currentHoverText.transform.position = Input.mousePosition;
         TMP_Text text = currentHoverText.GetComponentInChildren<TMP_Text>();
-        text.text = content;
+        RectTransform textRect = (RectTransform)text.transform;
         RectTransform rect = (RectTransform)currentHoverText.transform;
+        float textWidth = Mathf.Min(textRect.rect.size.x, Display.main.renderingWidth - textRect.position.x);
+        textRect.sizeDelta = new Vector2(textWidth, textRect.rect.size.y);
+        text.text = content;
         yield return null;
         Debug.Log("Setting hovertext bounds: " + text.textBounds.size);
         rect.sizeDelta = (Vector2)text.textBounds.size + new Vector2(22, 22);
 
+    }
+
+    private Vector3 previousMousePos = new();
+    private bool IsHoveredOver()
+    {
+        bool result = PointInBounds(Input.mousePosition) && gameObject.activeInHierarchy && previousMousePos == Input.mousePosition;
+        previousMousePos = Input.mousePosition;
+        return result;
+    }
+
+    private bool PointInBounds(Vector2 point)
+    {
+        if(point.x < bounds.min.x || point.x > bounds.max.x || point.y < bounds.min.y || point.y > bounds.max.y) { return false; }
+        return true;
     }
 
 
